cfbc79a17c3aea083aaec8d2e21c36eb9fcd0022
starseed777 <waress66@live.com>
2020-11-23 01:14:17 -0500

    Automation script that backs up DB to AWS S3

---
+++ aws-s3.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+DATE=$(date +H%-%M-%S)
+BACKUP=db-$DATE.sql
+DB_HOST=$1
+DB_PASSWORD=$2
+DB_NAME=$3
+AWS_SECRET=$4
+BUCKET_NAME=$5
+
+mysqldump -u root -h $DB_HOST -p$DB_PASSWORD $DB_NAME > /tmp/$BACKUP && \
+
+export AWS_ACCESS_KEY_ID=AKIAUXJBPIZDE2TTN3LA && \
+export AWS_SECRET_ACCESS_KEY=$AWS_SECRET && \
+echo "$BACKUP uploading"
+aws s3 cp /tmp/db-$DATE.sql s3://$BUCKET_NAME/$BACKUP
+
